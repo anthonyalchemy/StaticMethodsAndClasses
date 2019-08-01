867e3e637e2c875a27051fc4ded3b3c37956127f
anthonyalchemy <anthony@alchemytechs.com>
2019-08-01 09:54:18 +0100

    Add project files.

---
+++ StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticMethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static method is a method belongs to class itself 
+
+            Console.WriteLine(Math.Sqrt(144)); //math belongs to c# so dont have to create instance of math class 
+
+            UsefulTools.SayHi("Mike");//static method being called 
+
+         
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticMethodsAndClasses/UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+    static class UsefulTools //will not be able to create an instance in the main method ie usefultools tools = new usefultools();
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello" + name);
+        }
+    }
+}
